ffe64292a37e87ed6467e81fb2d7f37f905aa1b4
Edwina Dsouza <115996056+edwinadsouza@users.noreply.github.com>
2024-08-06 19:38:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Series AP/series-ap.py
@@ -0,0 +1,32 @@
+
+class Solution:
+    def nthTermOfAP(self, a1 : int, a2 : int, n : int) -> int:
+        d = a2 - a1
+        result = a1 + (n-1) * d
+        return result 
+        
+
+
+
+#{ 
+ # Driver Code Starts
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        a1 = int(input())
+        
+        
+        a2 = int(input())
+        
+        
+        n = int(input())
+        
+        obj = Solution()
+        res = obj.nthTermOfAP(a1, a2, n)
+        
+        print(res)
+        
+
+# } Driver Code Ends
