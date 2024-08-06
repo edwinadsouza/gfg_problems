fd4b1f616c6e33aeb6ca3232b942243a0bb513ae
Edwina Dsouza <115996056+edwinadsouza@users.noreply.github.com>
2024-08-06 19:59:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Multiplication Table/multiplication-table.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    def getTable(self,N):
+        table = []
+        for i in range(1,11):
+            result = N*i
+            table.append(result)
+        return table   
+        # code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ans = ob.getTable(N)
+        for i in ans:
+            print(i,end=" ")
+        print()
+# } Driver Code Ends
